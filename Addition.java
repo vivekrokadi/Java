702875f557de18bf993f351adcd51a64efa68927
vivekrokadi <vivekrokadi@gmail.com>
2026-01-09 18:47:50 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Addition {
+    public static void main(String []args){
+        Scanner obj = new Scanner(System.in);
+        String name;
+        System.out.println("Enter your name ");
+        name = obj.nextLine();
+        System.out.println("Name: "+name);
+    }
+}

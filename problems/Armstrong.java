c6a3d1930a303932691e586f42c2b431ea56ca8e
vivekrokadi <vivekrokadi@gmail.com>
2026-01-15 21:21:03 +0530

    Problem added

---
+++ problems/Armstrong.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int digits = 0;
+        int temp = num;
+
+        if (num < 0) {
+            System.out.println("Not an Armstrong Number");
+            sc.close();
+            return;
+        }
+
+        while (temp != 0) {
+            digits++;
+            temp = temp/10;
+        }
+
+        temp = num;
+        int sum = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp = temp/10;
+        }
+
+        if (original == sum) {
+            System.out.println("Armstrong No.");
+        } else {
+            System.out.println("Not a Armstrong No.");
+        }
+    }
+}
